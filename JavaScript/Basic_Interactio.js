055cd2d3162bbb82b915d75dd10ea2b3b158a20c
LuFipe <luizfelipecastrocoelho@gmail.com>
2021-04-25 17:32:27 -0300

    GradExp ADD
    tab about Graduation and Tab - config

--- JavaScript/Basic_Interactio.js
+++ JavaScript/Basic_Interactio.js
@@ -6,10 +6,12 @@ function dspSobre(){
 	var soft = window.document.getElementById("softGrid_ID");
 	var hobby = window.document.getElementById("hobbieGrid_ID");
 	var cont = window.document.getElementById("contGrid_ID");
+	var expGrad = window.document.getElementById("divExpGrad_ID");
 
 	soft.style.display = "none";
 	hobby.style.display = "none";
 	cont.style.display = "none";
+	expGrad.style.display = "none";
 	about.style.display = "flex";
 }
 function dspSoft(){
@@ -18,10 +20,12 @@ function dspSoft(){
 	var soft = window.document.getElementById("softGrid_ID");
 	var hobby = window.document.getElementById("hobbieGrid_ID");
 	var cont = window.document.getElementById("contGrid_ID");
+	var expGrad = window.document.getElementById("divExpGrad_ID");
 	
 	hobby.style.display = "none";
 	cont.style.display = "none";
 	about.style.display = "none";
+	expGrad.style.display = "none";
 	soft.style.display = "flex";
 }
 function dspHobby(){
@@ -30,10 +34,12 @@ function dspHobby(){
 	var soft = window.document.getElementById("softGrid_ID");
 	var hobby = window.document.getElementById("hobbieGrid_ID");
 	var cont = window.document.getElementById("contGrid_ID");
+	var expGrad = window.document.getElementById("divExpGrad_ID");
 	
 	cont.style.display = "none";
 	about.style.display = "none";
 	soft.style.display = "none";
+	expGrad.style.display = "none";
 	hobby.style.display = "flex";
 }
 function dspCont(){
@@ -42,9 +48,26 @@ function dspCont(){
 	var soft = window.document.getElementById("softGrid_ID");
 	var hobby = window.document.getElementById("hobbieGrid_ID");
 	var cont = window.document.getElementById("contGrid_ID");
+	var expGrad = window.document.getElementById("divExpGrad_ID");
 
 	about.style.display = "none";
 	soft.style.display = "none";
 	hobby.style.display = "none";
+	expGrad.style.display = "none";
 	cont.style.display = "flex";
+}
+
+function dspExpGrad(){
+	//window.alert("Entrou Ab");
+	var about = window.document.getElementById("aboutGrid_ID");
+	var soft = window.document.getElementById("softGrid_ID");
+	var hobby = window.document.getElementById("hobbieGrid_ID");
+	var cont = window.document.getElementById("contGrid_ID");
+	var expGrad = window.document.getElementById("divExpGrad_ID");
+
+	soft.style.display = "none";
+	hobby.style.display = "none";
+	cont.style.display = "none";
+	about.style.display = "none";
+	expGrad.style.display = "flex";
 }
